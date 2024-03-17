0ed847f47e23e7ca1dc737773fa4f6ebe04ff4e3
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2024-03-17 13:45:28 +0200

    io functions docs
    Docstrings for all function in input.py and output.py

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,34 @@
 def read_from_console():
+    """
+    Reads data from console
+
+    Returns:
+        str: data from console
+    """
     pass
 
 
 def read_from_file(file_name):
+    """
+    Reads data from file using python build-in python methods
+
+    Args:
+        file_name (str): file to read data from
+
+    Returns:
+        str: data from file
+    """
     pass
 
 
 def read_from_file_with_pandas(file_name):
+    """
+    Reads data from file using pandas
+
+    Args:
+        file_name (str): file to read data from
+
+    Returns:
+        str: data from file
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,18 @@
 def write_to_console(text):
+    """
+    Write text to console
+
+    Args:
+        text (str): text to print to console
+    """
     pass
 
 
 def write_to_file(text, file_name):
+    """
+    Write text to file using build-in python methods
+
+    Args:
+        text (str): text to write to a file
+    """
     pass
