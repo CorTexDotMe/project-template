b1ba9775a1c688b44c8b88fd267c262254176025
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2024-03-17 13:45:47 +0200

    use io function in main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import app.io.input
+import app.io.output
 
 
 def main():
-    pass
+    console_text = app.io.input.read_from_console()
+    file_text = app.io.input.read_from_file("data/input.txt")
+    pandas_file_text = app.io.input.read_from_file_with_pandas("data/input.txt")
+
+    app.io.output.write_to_console(f"From console: {console_text}")
+    app.io.output.write_to_console(f"From file: {file_text}")
+    app.io.output.write_to_console(f"From file with pandas: {pandas_file_text}")
+
+    app.io.output.write_to_file(console_text, "data/output_console_text.txt")
+    app.io.output.write_to_file(file_text, "data/output_file_text.txt")
+    app.io.output.write_to_file(pandas_file_text, "data/output_pandas_file_text.txt")
 
 
 if __name__ == '__main__':
